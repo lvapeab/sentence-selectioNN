914d9c080c99bd94fc68419acebf8465b1e4b300
Alvaro <lvapeab@gmail.com>
2016-12-08 10:28:17 +0100

    Dirty hack for experiments

--- main.py
+++ main.py
@@ -306,14 +306,13 @@ def semisupervised_selection(params):
         pool_filename_src = new_pool_filename_src
         pool_filename_trg = new_pool_filename_trg
 
-        #Dirty hack for faster experimentation
-        if i == 0:
-            params['INSTANCES_TO_ADD']*=2
-
         if len(neutral_lines_src) < 2*params['INSTANCES_TO_ADD']:
             logger.warning("We got out of neutral sentences (from the pool) to classify!. Stopping the process.")
             break
-
+        #Dirty hack for faster experimentation
+        if i == 1:
+            params['INSTANCES_TO_ADD'] = 100000
+                         
 def buildCallbacks(params, model, dataset):
     """
         Builds the selected set of callbacks run during the training of the model
